ff01b78b49aa18d93038ed16c51d46b872fc89e7
Jan Stamer <jan.stamer@red6-es.de>
2014-04-28 17:53:00 +0200

    Show blockers only if there are any.

--- widgets/sonar/sonar.js
+++ widgets/sonar/sonar.js
@@ -2,13 +2,24 @@ widget = {
 	onData: function (el, data) {
 
 		function directionHmtl (obj){
-	        return '<span class="direction-'+ obj.direction + '">&#187;</span>';
+            if (obj.direction === 'up') {
+                return '<span class="direction-up">&#8593;</span>';
+            } else {                
+                return '<span class="direction-down">&#8595;</span>';
+            }
 	    };
 
         $('.projectName', el).text(data.projectName);
         $('.coverage', el).html(data.coverage.value + '%' + directionHmtl(data.coverage));
         $('.linesOfCode', el).html(data.linesOfCode.value + directionHmtl(data.linesOfCode));
         $('.technicalDebt', el).html(data.technicalDebt.value + directionHmtl(data.technicalDebt));
+
+        if (data.blockerCount.value) {
+            $('.blockerCount', el).html(data.blockerCount.value + directionHmtl(data.coverage));
+            $('.blocker-alert', el).show();
+        } else {
+            $('.blocker-alert', el).hide();
+        }     
     },
     onError: function (el, data) {
         var $error = $('<div class="container"><img src="images/warning.png"></div>');
