0913d4f49986bf82552c76ac862cd3c6a51091d9
Jan Stamer <jan.stamer@red6-es.de>
2014-05-06 21:58:04 +0200

    Fixed merge errors.

--- jobs/jenkins-builds/jenkins-builds.js
+++ jobs/jenkins-builds/jenkins-builds.js
@@ -1,7 +1,6 @@
 module.exports = function (config, dependencies, job_callback) {
 	var async = require('async');
     var logger = dependencies.logger;
-<<<<<<< HEAD
     var moment = dependencies.moment;
 
 	function getBuildInfos (build, callback) {
@@ -28,11 +27,6 @@ module.exports = function (config, dependencies, job_callback) {
 
             callback(null, buildData);
         });
-=======
-
-	function getBuildInfos (build, callback) {
-        callback(null, build);
->>>>>>> 2cbd108a6e7ea728699a46d30d74815b3a8a4221
     };
 
     var jenkinsJobUrl = config.serverUrl + '/job/' + config.job + '/api/json'
@@ -46,23 +40,6 @@ module.exports = function (config, dependencies, job_callback) {
     };
 
     dependencies.easyRequest.JSON(options, function (error, jobData) {
-<<<<<<< HEAD
-        if (error) {
-          	var err_msg = error || "ERROR: Couldn't access the job at " + options.url;
-           	logger.error(err_msg);
-           	return job_callback(err_msg);
-        }
-
-        var lastBuilds = jobData.builds.slice(0, Math.min(3, jobData.builds.length));
-
-        async.map(lastBuilds, getBuildInfos, function (err, lastBuildsInfos) {
-	        var data = {
-	         	name: jobData.displayName,
-	           	builds: lastBuildsInfos
-            };
-            return job_callback(null, data);
-        });
-=======
             if (error) {
             	var err_msg = error || "ERROR: Couldn't access the job at " + options.url;
             	logger.error(err_msg);
@@ -79,7 +56,5 @@ module.exports = function (config, dependencies, job_callback) {
 
 	            return job_callback(null, data);
 			});
-
->>>>>>> 2cbd108a6e7ea728699a46d30d74815b3a8a4221
     });
 };
--- widgets/jenkins-builds/jenkins-builds.js
+++ widgets/jenkins-builds/jenkins-builds.js
@@ -2,25 +2,9 @@ widget = {
 	//runs when we receive data from the job
 	onData: function (el, data) {
 		var html = '<h2>BUILDS</h2>';
-
-			/**
-	<div class="build build-successfull">
-		HUM-23 <span class="build-time">2 minutes ago</span>
-	</div>
-	<div class="build build-successfull">
-		HUM-23 <span class="build-time">2 minutes ago</span>
-	</div>
-	<div class="build build-failed">
-		HUM-23 <span class="build-time">2 minutes ago</span>
-	</div>
-	*/
 		
 		$.each(data.builds, function (index, build) {
-<<<<<<< HEAD
 			html += '<div class="build build-' + build.result + '">' + build.fullName + ' <span class="build-time">' + build.timeAgo + '</span>' + '</div>';
-=======
-			html += '<div class="build build-successfull">' + data.name + '-' + build.number + ' <span class="build-time">2 minutes ago</span>' + '</div>';
->>>>>>> 2cbd108a6e7ea728699a46d30d74815b3a8a4221
 		});
 
 		$('.content', el).html(html);
