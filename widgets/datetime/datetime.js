5f9bb0c215640cfaa7e9912b9f7a48a1fe72a216
Jan Stamer <jan.stamer@red6-es.de>
2014-09-04 21:30:59 +0200

    Fixed datetime without moment.js

--- widgets/datetime/datetime.js
+++ widgets/datetime/datetime.js
@@ -1,32 +1,46 @@
 widget = {
 	//runs when we receive data from the job
 	onData: function (el, data) {
-        try {
-            moment();
-        } catch (e) {
-          console && console.log('ERROR: Moment.js not defined. Please add "moment-with-locales.min.js" as "customJS" to your board.');
-          return;
-        }
-
         if (!$.data(el, "datetime-initialized")) {
-            var updateTime = function () {
-                var template = _.template($('.template', el).html());
 
-                // UTC Time
-                var time = moment().add(new Date().getTimezoneOffset(), 'minutes');
+            var updateTime;
+            try {
+                // Check if moment is defined
+                moment();
 
-                // Add difference to UTC if set correctly
-                if (data.differenceUTC && _.isNumber(data.differenceUTC)) {
-                    time = time.add(data.differenceUTC, 'hours');
-                }
+                updateTime = function () {
+                    var template = _.template($('.template', el).html());
 
-                var datetimeData = {
-                    time: time.lang(data.lang).format('HH:mm'),
-                    date: moment().lang(data.lang).format('L')
+                    // UTC Time
+                    var time = moment().add(new Date().getTimezoneOffset(), 'minutes');
+
+                    // Add difference to UTC if set correctly
+                    if (data.differenceUTC && _.isNumber(data.differenceUTC)) {
+                        time = time.add(data.differenceUTC, 'hours');
+                    }
+
+                    var datetimeData = {
+                        time: time.lang(data.lang).format('HH:mm'),
+                        date: moment().lang(data.lang).format('L')
+                    };
+
+                    $('.content', el).html(template(datetimeData));
                 };
+            } catch (e) {
+                console && console.log('ERROR: Moment.js not defined. Please add "moment-with-locales.min.js" as "customJS" to your board.');
+                 updateTime = function () {
+                    var template = _.template($('.template', el).html());
 
-                $('.content', el).html(template(datetimeData));
-            };
+                    var now = new Date();
+                    var datetimeData = {
+                        time: now.getHours() + ":" + now.getMinutes(),
+                        date: new Date().toLocaleDateString()
+                    };
+
+                    $('.content', el).html(template(datetimeData));
+                };
+            }
+
             setInterval(updateTime, (10 * 1000));
             updateTime();
             $.data(el, "datetime-initialized", true);
