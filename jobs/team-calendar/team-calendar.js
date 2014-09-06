31d1055c75f7a4f9340e3d42534ce56e5d1854b3
Jan Stamer <jan.stamer@red6-es.de>
2014-09-06 21:15:10 +0200

    Display regular ical events on team calendar.

--- jobs/team-calendar/team-calendar.js
+++ jobs/team-calendar/team-calendar.js
@@ -5,6 +5,19 @@ var ical = require('ical'),
 module.exports = function (config, dependencies, job_callback) {
     var logger = dependencies.logger;
 
+    var days = [];
+    for (i = 1; i < 6; i++) {
+        var weekDay = moment().day(i).hours(0).minutes(0).seconds(0).milliseconds(0);
+
+        var day = {
+            name: weekDay.lang(config.lang).format('dddd'),
+            date: weekDay.lang(config.lang).format('L'),
+            dateMoment: weekDay,
+            events: []
+        };
+        days.push(day);
+    }
+
     ical.fromURL(config.calendarUrl, {}, function(err, data) {
         if (err){
             logger.error(err);
@@ -12,35 +25,33 @@ module.exports = function (config, dependencies, job_callback) {
             return;
         }
 
-        _.each(data, function(event) {
-            logger.error(JSON.stringify(event));
-            logger.error("START: " + moment(event.start).format());
-            logger.error("END: " + moment(event.end).format());
+        var firstDayOfTheWeek = days[0].dateMoment.toDate();
+        var lastDayOfTheWeek = days[4].dateMoment.toDate();
+
+        var eventsOfTheWeek = _.filter(data, function(event) {
+            return event.end >= firstDayOfTheWeek && event.start <= lastDayOfTheWeek;
         });
 
-//        var events = _.sortBy(data, function(event) { return event.start; });
-//        events = _.filter(events, function(event) { return event.end >= new Date(); });
+        _.each(eventsOfTheWeek, function(event) {
+            _.each(days, function (day) {
+                var dayOfWeek = day.dateMoment.toDate().getDay();
+                if (event.start.getDay() <= dayOfWeek && dayOfWeek <= event.end.getDay()) {
+                    day.events.push({
+                        startTime: moment(event.start).format('HH:mm'),
+                        endTime: moment(event.end).format('HH:mm'),
+                        summary: event.summary
+                    });
+                }
+            });
+        });
 
-
-        logger.error("&&&&&&&&&&&&%%%%%%%%%%");
+        var data = {
+            title: config.widgetTitle,
+            lang: config.lang,
+            differenceUTC: config.differenceUTC,
+            days: days
+        };
+        job_callback(null, data);
     });
 
-    var days = [];
-    for (i = 1; i < 6; i++) {
-        var weekDay = moment().day(i);
-
-        var day = {
-            name: weekDay.lang(config.lang).format('dddd'),
-            date: weekDay.lang(config.lang).format('L')
-        };
-        days.push(day);
-    }
-
-    var data = {
-        title: config.widgetTitle,
-        lang: config.lang,
-        differenceUTC: config.differenceUTC,
-        days: days
-    };
-    job_callback(null, data);
 };
